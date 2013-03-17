3e79b73eb2ae6ef85e9b8dbf516ee7e3e9c43a1b
forcemantis <amitjain2050@gmail.com>
2013-03-17 09:22:00 +0530

    Added Missing code in source and Recompiled jar

--- DeployPurge/src/com/salesforce/ant/DeployPurge.java
+++ DeployPurge/src/com/salesforce/ant/DeployPurge.java
@@ -1,5 +1,14 @@
 package com.salesforce.ant;
+import com.salesforce.ant.DeployTask;
+public class DeployPurge extends DeployTask
+{
+    public DeployPurge()
+    {
+        this.deployOptions.setPurgeOnDelete(false);
+    }
 
-public class DeployPurge {
-
+    public void setPurgeOnDelete(boolean purgeOnDelete) 
+    {
+        this.deployOptions.setPurgeOnDelete(purgeOnDelete);
+    }
 }
